0baa0af2768ba3f9cf35a48d2136e8d06a1c0277
gavin <gavinrvsmith@gmail.com>
2025-02-04 22:34:34 -0800

    geo hw2

--- geometry/main.py
+++ geometry/main.py
@@ -10,6 +10,8 @@ class Point:
         self.y = y
     def __repr__(self):
         return str(self.x) + ',' + str(self.y)
+    def __eq__(self, value):
+        return self.x == value.x and self.y == value.y
 
 class Line:
     p1: Point
@@ -43,6 +45,9 @@ def dist(p1: Point, p2: Point):
 def intersect(l1: Line, l2: Line):
     [p1, p2] = [l1.p1, l1.p2]
     [p3, p4] = [l2.p1, l2.p2]
+    if ((p1.x - p2.x)*(p4.y - p3.y) - (p2.y - p1.y)*(p3.x - p4.x)) == 0 or \
+        (p1.x*p3.y - p1.x*p4.y - p1.y*p3.x + p1.y*p4.x - p2.x*p3.y + p2.x*p4.y + p2.y*p3.x - p2.y*p4.x) == 0:
+        return Point(0,0)
     return Point(-((p1.x - p2.x)*(p3.y*p4.x - p3.x*p4.y) - (p3.x - p4.x)*(p1.y*p2.x - p1.x*p2.y))/((p1.x - p2.x)*(p4.y - p3.y) - (p2.y - p1.y)*(p3.x - p4.x)),
                    -(-p1.x*p2.y*p3.y + p1.x*p2.y*p4.y + p1.y*p2.x*p3.y - p1.y*p2.x*p4.y + p1.y*p3.x*p4.y - p1.y*p3.y*p4.x - p2.y*p3.x*p4.y + p2.y*p3.y*p4.x)/(p1.x*p3.y - p1.x*p4.y - p1.y*p3.x + p1.y*p4.x - p2.x*p3.y + p2.x*p4.y + p2.y*p3.x - p2.y*p4.x))
 
@@ -95,7 +100,7 @@ def ybetween(p1: Point, p2: Point, p3: Point):
 def slope(l: Line):
   [p1,p2] = [l.p1, l.p2]
   if p2.x == p1.x:
-    return False
+    return math.inf
   return (p2.y-p1.y)/(p2.x-p1.x)
 
 #A list of points called p
@@ -134,17 +139,22 @@ def rayintseg(ray: Line, lineSeg: Line):
 
     if p1.x <= p2.x: #ray is going right
         if (inter.x >= p1.x):
-            return True
+            return ybetween(p3,inter,p4)
         
     else: #ray left
         if (inter.x <= p1.x):
-            return True
+            return ybetween(p3,inter,p4)
 
 # plotpoly(p)
 
 #3
 def segintseg(lineSeg1: Line, lineSeg2: Line):
-    return xbetween(lineSeg1.p1,intersect(lineSeg1, lineSeg2),lineSeg2.p2)
+    int = intersect(lineSeg1, lineSeg2)
+    x1 = xbetween(lineSeg1.p1,int,lineSeg1.p2)
+    x2 = xbetween(lineSeg2.p1,int,lineSeg2.p2)
+    y1 = ybetween(lineSeg1.p1,int,lineSeg1.p2)
+    y2 = ybetween(lineSeg2.p1,int,lineSeg2.p2)
+    return x1 and x2 and y1 and y2
 
 #4
 def areParallel(l1: Line, l2: Line):
@@ -188,11 +198,12 @@ def countCrossingVerticies(ray: Line, p: list[Point]):
     for i in range(len(p)):
         if pointIsOnLine(p[i], ray) and p[i].y > ray.p1.y:
             #ray goes thru vertex
-            s1 = slope(Line(ray.p1, p[i-1]))
+            s1 = slope(Line(p[i], p[i-1]) if p[i].x <= p[i-1].x else Line(p[i-1], p[i]))
             s2 = slope(ray)
-            s3 = slope(Line(ray.p1, p[(i+1) % len(p)]))
+            n = p[(i+1) % len(p)]
+            s3 = slope(Line(p[i], n) if p[i].x <= n.x else Line(n, p[i]))
 
-            if s1 > s2 > s3 or s1 < s2 < s3: count += 1
+            if not (s1 > s2 > s3 or s1 < s2 < s3): count += 1
     return count
 
 #8
@@ -211,7 +222,7 @@ def IsOnBoundaryOfPolygon(p1: Point, p: list[Point]):
 def IsInsidePolygon(p1: Point, p: list[Point]):
     if IsOnBoundaryOfPolygon(p1, p): return True
 
-    ray = getNonParallelLine(d)
+    ray = getNonParallelLine(p)
     ray = Line(p1, Point(p1.x+ray.p2.x, p1.y+ray.p2.y))
 
     crosses = countCrossingVerticies(ray, p)
@@ -222,18 +233,52 @@ def IsInsidePolygon(p1: Point, p: list[Point]):
     return crosses % 2 == 1
 
 
-#the polygon from problem 3d
-d = [Point(*p) for p in [[16,42],[46,141],[192,218],[231,89],[362,254],[417,15],[364,94],[110,45],[184,141]]]
-# plotpoly(d)
+# #the polygon from problem 3d
+# d = [Point(*p) for p in [[16,42],[46,141],[192,218],[231,89],[362,254],[417,15],[364,94],[110,45],[184,141]]]
+# # plotpoly(d)
 
-inside = np.zeros((260, 430))
+# inside = np.zeros((260, 430))
 
-for y in range(0, 430):
-    for x in range(0, 260):
-        inside[x,y] = IsInsidePolygon(Point(y,x), d)
+# for y in range(0, 430):
+#     for x in range(0, 260):
+#         inside[x,y] = IsInsidePolygon(Point(y,x), d)
 
 
-plt.imshow(inside, cmap='gray', interpolation='nearest')
+# plt.imshow(inside, cmap='gray', interpolation='nearest')
+# plt.show()
 
+def segIntSegNotAtEnd(lineSeg1: Line, lineSeg2: Line):
+    int = intersect(lineSeg1, lineSeg2)
+
+    return segintseg(lineSeg1, lineSeg2) and not (lineSeg1.p1 == lineSeg2.p1 or lineSeg1.p1 == lineSeg2.p2 or lineSeg1.p2 == lineSeg2.p1 or lineSeg1.p2 == lineSeg2.p2 or \
+        int == lineSeg1.p1 or int == lineSeg2.p1)
+
+def IsInVertexKernel(p1: Point, P: list[Point]):
+    if not IsInsidePolygon(p1, P): return False
+    for vertex in P:
+        line = Line(vertex, p1)
+        crosses = 0
+        for edge in iterateEdges(P):
+            if segIntSegNotAtEnd(line, edge):
+                crosses += 1 
+        if crosses > 2: return False
+    return True
+
+N = 300
+P = [Point(p[0] * N/10, N - p[1] * N/10) for p in [[0,0],[10,0],[10,10],[1,10],[8,8],[8,2],[5,6],[2,2],[0,9]]]
+plotpoly(P)
+
+IsInsidePolygon(Point(90,90), P)
+
+kernel = np.zeros((N, N))
+
+for y in range(0, N):
+    for x in range(0, N):
+        kernel[y,x] = IsInVertexKernel(Point(x, y), P)
+        # kernel[y,x] = IsInsidePolygon(Point(x,y), P)
+
+
+
+plt.imshow(kernel, cmap='gray', interpolation='nearest')
 
 plt.show()
